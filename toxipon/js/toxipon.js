cb74d52f6250a8861e19c71229f442af7fb8831d
Juan Esteban Gaviria Mena <gaviria1224@outlook.com>
2023-04-03 11:44:46 -0500

    avance 1.

--- toxipon/js/toxipon.js
+++ toxipon/js/toxipon.js
@@ -1,7 +1,29 @@
-let botonSeleccionarMascota = document.getElementById(
-  "boton-seleccionar-mascota"
-);
-function manejadorSeleccionarMascota() {
-  alert("seleccionaste fuego");
+console.log("antes de iniciar")
+
+function iniciarJuego() {
+  let botonSeleccionarMascota = document.getElementById("boton-seleccionar-mascota");
+  botonSeleccionarMascota.addEventListener("click", manejadorSeleccionarMascotaJugador);
+  console.log("inicio")
 }
-botonSeleccionarMascota.addEventListener("click", manejadorSeleccionarMascota);
+
+
+function manejadorSeleccionarMascotaJugador() {
+  let inputHipodoge = document.getElementById("hipodoge")
+  let inputCapipepo = document.getElementById("capipepo")
+  let inputRatigueya = document.getElementById("ratigueya")
+  let spanMascotaJugador = document.getElementById("mascota-jugador")
+  console.log(inputHipodoge)
+
+  if (inputHipodoge.Checked) {
+    spanMascotaJugador.innerHTML = "hipodoge";
+  } else if (inputCapipepo.Checked) {
+    spanMascotaJugador.innerHTML = "capipepo";
+    console.log("")
+  } else if (inputRatigueya.Checked) {
+    spanMascotaJugador.innerHTML = "ratigueya";
+  } else {
+    alert("No seleccionaste mascota");
+  }
+  
+}
+window.addEventListener("load", iniciarJuego)
