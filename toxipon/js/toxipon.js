98eb5f7571f4df3fcddc3d81c7ff16417de46a3c
Juan Esteban Gaviria Mena <gaviria1224@outlook.com>
2023-04-03 15:12:22 -0500

    Seleecionar mascotas, listo.

--- toxipon/js/toxipon.js
+++ toxipon/js/toxipon.js
@@ -1,29 +1,51 @@
-console.log("antes de iniciar")
 
-function iniciarJuego() {
-  let botonSeleccionarMascota = document.getElementById("boton-seleccionar-mascota");
-  botonSeleccionarMascota.addEventListener("click", manejadorSeleccionarMascotaJugador);
-  console.log("inicio")
+function obtenerNumeroEnRango(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 
-function manejadorSeleccionarMascotaJugador() {
+function iniciarJuego(){
+
+  let botonSeleccionarMascotaJugador = document.getElementById("boton-seleccionar-mascota")
+  botonSeleccionarMascotaJugador.addEventListener ("click", manejadorSeleccionarMascotaJugador)
+
+}
+
+function manejadorSeleccionarMascotaJugador(){
+
   let inputHipodoge = document.getElementById("hipodoge")
   let inputCapipepo = document.getElementById("capipepo")
   let inputRatigueya = document.getElementById("ratigueya")
   let spanMascotaJugador = document.getElementById("mascota-jugador")
-  console.log(inputHipodoge)
 
-  if (inputHipodoge.Checked) {
-    spanMascotaJugador.innerHTML = "hipodoge";
-  } else if (inputCapipepo.Checked) {
-    spanMascotaJugador.innerHTML = "capipepo";
-    console.log("")
-  } else if (inputRatigueya.Checked) {
-    spanMascotaJugador.innerHTML = "ratigueya";
+  if(inputHipodoge.checked) {
+    spanMascotaJugador.innerHTML = "hipodoge"
+  } else if(inputCapipepo.checked) {
+    spanMascotaJugador.innerHTML = "capipepo"
+  } else if(inputRatigueya.checked) {
+    spanMascotaJugador.innerHTML = "ratigueya"
   } else {
-    alert("No seleccionaste mascota");
+    alert("no seleccionaste ninguna mascota")
   }
-  
+
+  manejadorSeleccionarMascotaEnemigo()
+
 }
+
+function manejadorSeleccionarMascotaEnemigo() {
+
+  let numeroAleatorio = obtenerNumeroEnRango(1,3)
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+  if (numeroAleatorio == 1) {
+    spanMascotaEnemigo.innerHTML = "hipodoge"
+  } else if (numeroAleatorio == 2) {
+    spanMascotaEnemigo.innerHTML = "capipepo"
+  } else if (numeroAleatorio == 3) {
+    spanMascotaEnemigo.innerHTML = "ratigueya"
+  }
+}
+
+
+
 window.addEventListener("load", iniciarJuego)
